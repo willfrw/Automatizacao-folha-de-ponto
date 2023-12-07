dcd19e37ac9fa4b73b32cebac0a654b6efdfd53d
William Wang <william.wang_dev@outlook.com>
2023-12-07 15:37:31 -0300

    updates

--- functions.py
+++ functions.py
@@ -1,26 +1,12 @@
+import os
 from openpyxl import Workbook, load_workbook
-
-from win32com import client
-
-app = client.DispatchEx("Excel.Application")
-
-app.Interactive = False
-
-app.Visible = False
-
-path = input("Coloque o caminho do arquivo: ")
-
-workbook = app.Workbooks.Open(path)
-workbook.ActiveSheet.ExportAsFixedFormat(0, path)
-workbook.Close()
-
+import win32com.client
 from datetime import datetime, timedelta
 
 def gerador_folhas_col(dados, modelo_folha_ponto):
     planilha_nomes = load_workbook(dados)
 
     aba_ativa_nomes = planilha_nomes.active
-
     for celula in aba_ativa_nomes["A"]:
         planilha_folha_ponto = load_workbook(modelo_folha_ponto)
         aba_ativa_folha = planilha_folha_ponto.active
@@ -31,14 +17,33 @@ def gerador_folhas_col(dados, modelo_folha_ponto):
         cargo = aba_ativa_nomes[f"C{linha_nome}"].value
         mat = aba_ativa_nomes[f"A{linha_nome}"].value
 
-        aba_ativa_folha["C5"] = nome
-        aba_ativa_folha["C6"] = cargo
-        aba_ativa_folha["G6"] = mat
+        aba_ativa_folha["C4"] = "NOME: " + str(nome)
+        aba_ativa_folha["C5"] = "CARGO: " + str(cargo)
+        aba_ativa_folha["G5"] = "MATRÍCULA: " + str(mat)
 
         nome_planilha = str(f"{setor}_{nome}.xlsx")
         planilha_folha_ponto.save(nome_planilha)
-        path = f"C:\Users\usuario\OneDrive\Documentos\GitHub\Automatizacao-folha-de-ponto\arquivos\testes com arquivos modelo\{setor}_{nome}.xlsx"
-        planilha_folha_ponto.ExportAsFixedFormat(0, path)
+    return 0
+
+# A função irá criar um arquivo em pdf com base no excel
+# Criar um loop for com o tamanho da lista com os arquivo para criar conforme percorsse o diretório
+def gerador_pdf(path):
+    excel = win32com.client.Dispatch("Excel.Application")
+    excel.Visible = False
+    excel.DisplayAlerts = False
+
+    workbook = excel.Workbooks.Open(path)
+    path_pdf = path.replace(".xlsx", ".pdf")
+
+    try:
+        workbook.ActiveSheet.ExportAsFixedFormat(0, path_pdf)
+    except Exception as e:
+        print(f"Erro ao exportar {path} para PDF: {str(e)}")
+    finally:
+        workbook.Close(False)
+        excel.Quit()
+
+    return 0
 
 
 
@@ -46,7 +51,7 @@ def gerador_folhas_col(dados, modelo_folha_ponto):
     print("Teste")
 
 meses = {"jan":("01/01/2024")}"""
-gerador_folhas_col("modelo-dados-teste.xlsx", "modelo-tabela-folha-ponto-teste.xlsx")
 
-#gerador_modelo_folha("modelo-tabela-ponto-teste.xlsx")
+
+
 
---
+++ main.py
@@ -0,0 +1,31 @@
+from functions import gerador_folhas_col, gerador_pdf
+import os
+
+print("Gerando")
+gerador_folhas_col("modelo-dados-teste.xlsx", "modelo-tabela-folha-ponto-teste.xlsx")
+
+lista_caminho = []
+
+dirlist = os.listdir(".")
+
+for file in dirlist:
+    filename = os.path.abspath(file)
+    lista_caminho.append(filename)
+
+for arquivo in lista_caminho:
+    # Se o arquivo for .xlsx imprime o caminho do arquivo
+    if arquivo[-5:] == ".xlsx" and arquivo[-10:] != "teste.xlsx":
+        gerador_pdf(arquivo)
+        os.remove(arquivo)
+
+print("Finalizado")
+# Falta colocar mudança de diretório para salvamento correto dos arquivos
+
+
+
+"""print("Gerador de folha de ponto")
+
+folha_de_ponto = input("Digite o caminho do modelo de folha de ponto: ")
+
+dados_func = input("Digite o caminho da planilha com os dados dos funcionários: ")
+"""
